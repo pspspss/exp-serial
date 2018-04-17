0df87c299fced6e58394805ade4b93aa6fd3e22c
Paulo Alves <juninhpaulo@hotmail.com>
2018-04-17 19:24:41 -0300

    Pong Serial

--- pong/sketch.js
+++ pong/sketch.js
@@ -1,52 +1,52 @@
 function Pallet(x, y, w, h, keyUp, keyDown) {
-	this.x = x;
-	this.y = y;
-	this.w = w;
-	this.h = h;
+	this.x = x
+	this.y = y
+	this.w = w
+	this.h = h
 
 	this.draw = function () {
-		rectMode(CENTER);
-		rect(this.x, this.y, this.w, this.h);
+		rectMode(CENTER)
+		rect(this.x, this.y, this.w, this.h)
 	}
 
 	this.move = function () {
-		if (keyIsDown(keyUp)) { this.y -= 3; }
-		if (keyIsDown(keyDown)) { this.y += 3; }
-		this.y = constrain(this.y, this.h / 2, height - this.h / 2);
+		if (keyIsDown(keyUp)) { this.y -= 3 }
+		if (keyIsDown(keyDown)) { this.y += 3 }
+		this.y = constrain(this.y, this.h / 2, height - this.h / 2)
 	}
 }
 
 function Ball(x, y, r) {
-	this.x = x;
-	this.y = y;
-	this.r = r;
-	this.vx = random([-2, 2]);
-	this.vy = random([-0.5, 0.5]);
+	this.x = x
+	this.y = y
+	this.r = r
+	this.vx = random([-2, 2])
+	this.vy = random([-0.5, 0.5])
 
 	this.draw = function () {
-		ellipse(this.x, this.y, this.r, this.r);
+		ellipse(this.x, this.y, this.r, this.r)
 	}
 
 	this.move = function () {
-		this.x += this.vx;
-		this.y += this.vy;
+		this.x += this.vx
+		this.y += this.vy
 	}
 
 	this.intersectPallet = function (pallet) {
 		if (this.y < pallet.y + pallet.h / 2 + this.r / 2 && this.y > pallet.y - pallet.h / 2 - this.r / 2 && this.r > abs(this.x - pallet.x)) {
-			this.vx = -this.vx * 1.01;
-			this.vy = -this.vy * random([1.10, 0.9, -0.9, 1.04, 0.8, 1.2, 5, 0.2]);
-			serial.write("b'l'");
+			this.vx = -this.vx * 1.01
+			this.vy = -this.vy * random([1.10, 0.9, -0.9, 1.04, 0.8, 1.2, 5, 0.2])
+			serial.write("b'l'")
 		}
 	}
 
 	this.restart = function () {
 
 		if (this.x < 0 || this.x > width || this.y < 0 || this.y > height) {
-			this.x = width / 2;
-			this.y = height / 2;
-			this.vx = 2;
-			this.vy = random(-0.2, 0.2);
+			this.x = width / 2
+			this.y = height / 2
+			this.vx = 2
+			this.vy = random(-0.2, 0.2)
 		}
 	}
 }
@@ -58,8 +58,8 @@ let pallets = []
 function setup() {
 	createCanvas(400, 400)
 
-	serial = new p5.SerialPort(document.location.hostname);
-	serial.open('/dev/ttyACM0');
+	serial = new p5.SerialPort(document.location.hostname)
+	serial.open('/dev/ttyACM0')
 
 	ball = new Ball(200, 200, 25)
 	pallets[0] = new Pallet(20, 200, 20, 100, 87, 83)
